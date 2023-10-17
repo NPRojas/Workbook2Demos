4cc8f840f404cb196d1ed1805f59bb94e5d2be04
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-17 12:55:11 -0400

    practice var

---
+++ demo1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var firstName;
+firstName = 'Nicole';
+
+var lastName;
+lastName = 'Rojas';
+
+var fullName;
+fullName = lastName + ', ' + firstName;
+
+var message;
+
+message = 'The user/s full name is: ' + fullName;
