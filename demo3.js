07b1f5aa0b6784a784f163aee8a34fcb0e5c42d7
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-17 13:38:41 -0400

    complete excercise 1

--- demo3.js
+++ demo3.js
@@ -1,3 +1,18 @@
+var idenfication;
+var userFullName;
+var gender;
+var dob;
+var license;
+var policy;
+
+idenfication = "3842564142";
+userFullName = "Miguel O'Hara";
+gender = "male";
+dob = "05/14/82";
+license = "3223878895"; 
+policy = "6588555455";
+
+
 var fullName;
 var city;
 var state;
