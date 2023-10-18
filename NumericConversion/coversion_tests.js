8c07472621b11628cee7b7fff26515375b4172d8
NPRojas <63315208+NPRojas@users.noreply.github.com>
2023-10-18 15:20:02 -0400

    completed numeric converstions excercise

---
+++ NumericConversion/coversion_tests.js
@@ -0,0 +1,27 @@
+// Description: This script tests various numeric 
+// conversion techniques
+
+const a = '  101.1  ';
+const b = '55';
+const c = '402 Stevens';
+const d = 'Number 5';
+
+// convert to intergers
+console.log(parseInt(a));
+console.log(parseInt(b));
+console.log(parseInt(c));
+console.log(parseInt(d));
+
+// convert to float
+console.log(parseFloat(a));
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
+
+
+// convert to number
+console.log(Number(a));
+console.log(Number(b));
+console.log(Number(c));
+console.log(Number(d));
+
